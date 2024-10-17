9f1f1f7237455c941feba996b994b225834724cd
Robert de S. Maio <robertmaio@outlook.com>
2024-10-16 21:21:43 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "../../Assets/image.png";
+import "./styles.css";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <div className="nav">
+          <img className="portfolio" alt="portfolio" src={logo}></img>
+          <ul>
+            <li>
+              <a
+                href="https://api.whatsapp.com/send?phone=22992843155"
+                target="_blank"
+                rel="whatsapp"
+              >
+                Contato
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://linkedin.com/in/robert-de-s-maio"
+                target="_blank"
+                rel="linkedin"
+              >
+                Sobre
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/RobertdeSMaio"
+                target="_blank"
+                rel="github"
+              >
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export { Header };
