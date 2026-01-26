99aebbe0274f125352f2434332a12e00d61fd688
Robert de S. Maio <robertmaio@outlook.com>
2026-01-26 15:18:20 -0300

    Add path

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Pages/Home";
+import App from "./Pages/Home/index.tsx";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(
