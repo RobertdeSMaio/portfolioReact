c2ed0ef2b8b640686b74fee65d66fc8e8b370122
Robert de S. Maio <robertmaio@outlook.com>
2026-02-20 20:50:16 -0300

    change mx-w

--- src/Pages/Home/App.tsx
+++ src/Pages/Home/App.tsx
@@ -49,7 +49,7 @@ export default function App() {
       <Header />
 
       <main className="relative z-10 flex-1 w-full bg-[#233338] pb-10 grid grid-cols-1 gap-10 md:grid-cols-2">
-        <div className="mx-auto bg-white p-6 md:p-12 mt-10 rounded-xl shadow-2xl">
+        <div className="max-w-4xl mx-auto bg-white p-6 md:p-12 mt-10 rounded-xl shadow-2xl">
           <section className="flex flex-col items-center">
             <img
               className="h-32 w-32 border-2 border-black rounded-full object-cover mb-6 shadow-md"
